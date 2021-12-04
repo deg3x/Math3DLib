1ad365ec9b507c160d8aebf4d113a593e72f6213
Konstantinos Benos <mpenos.ks@gmail.com>
2021-12-04 02:33:58 +0200

    Add circle and sphere SDF implementations

--- Math3D/Core/Solvers/sdf.cpp
+++ Math3D/Core/Solvers/sdf.cpp
@@ -1,1 +1,12 @@
 #include "sdf.h"
+#include "vector.h"
+
+float CircleSDF(math3d::Vector<float, 2> center, float radius, math3d::Vector<float, 2> point)
+{
+	return (center - point).Magnitude() - radius;
+}
+
+float SphereSDF(math3d::Vector<float, 3> center, float radius, math3d::Vector<float, 3> point)
+{
+	return (center - point).Magnitude() - radius;
+}
--- Math3D/Core/Solvers/sdf.h
+++ Math3D/Core/Solvers/sdf.h
@@ -1,1 +1,4 @@
 #pragma once
+
+float CircleSDF(math3d::Vector<float, 2> center, float radius, math3d::Vector<float, 2> point);
+float SphereSDF(math3d::Vector<float, 3> center, float radius, math3d::Vector<float, 3> point);
